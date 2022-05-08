46046495936451c12407080a7c1a07cf6eeaf0ea
Angelix <komuro.game@gmail.com>
2022-05-07 20:44:16 -0700

    Register Function
    Added basic register function for NPC.

--- Server/Anomaly/_Function.lua
+++ Server/Anomaly/_Function.lua
@@ -5,3 +5,11 @@ if not Anomaly then
 	Anomaly.Conf.Text = {NPC = {}}
 	Anomaly.Lobby = {}
 end
+
+Anomaly.Register = function(Player, NPC)
+	if not Anomaly.Conf.Enable then
+		HelpInfo(Player, NPC, Anomaly.Conf.Text.NPC.NotEnabled:format(Anomaly.Conf.NameNPC, Anomaly.Conf.Name))
+		return LUA_FALSE
+	end
+	return LUA_TRUE
+end
