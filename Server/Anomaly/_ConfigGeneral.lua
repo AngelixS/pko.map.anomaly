f87a5da4e736acb7325fef7d600ab07cfc6501ed
Angelix <komuro.game@gmail.com>
2022-05-07 20:51:26 -0700

    Level Requirement
    Added checks for level requirements and it's respective text.

--- Server/Anomaly/_ConfigGeneral.lua
+++ Server/Anomaly/_ConfigGeneral.lua
@@ -19,3 +19,6 @@
 	-- ***************************************************************************************************************************** --
 	Anomaly.Conf.Text.NPC.MainTalk = '%s: We found [%s] and it\'s mysterious connection to another realm and endless energy is coming through.'
 	Anomaly.Conf.Text.NPC.NotEnabled = '%s: The strange energy coming through is not stable enough, so entrance to [%s] is prohibited at this moment.'
+	Anomaly.Conf.Text.NPC.AboveLevel = '%s: You need to be above level %d to enter [%s].'
+	Anomaly.Conf.Text.NPC.BelowLevel = '%s: You need to be below level %d to enter [%s].'
+	
--- Server/Anomaly/_Function.lua
+++ Server/Anomaly/_Function.lua
@@ -11,5 +11,14 @@ Anomaly.Register = function(Player, NPC)
 		HelpInfo(Player, NPC, Anomaly.Conf.Text.NPC.NotEnabled:format(Anomaly.Conf.NameNPC, Anomaly.Conf.Name))
 		return LUA_FALSE
 	end
+	local PlayerLevel = Lv(Player)
+	if Anomaly.Conf.Require.MinLevel ~= 0 and PlayerLevel < Anomaly.Conf.Require.MinLevel then
+		HelpInfo(Player, NPC, Anomaly.Conf.Text.NPC.AboveLevel:format(Anomaly.Conf.NameNPC, Anomaly.Conf.Require.MinLevel, Anomaly.Conf.Name))
+		return LUA_FALSE
+	end
+	if Anomaly.Conf.Require.MaxLevel ~= 0 and PlayerLevel > Anomaly.Conf.Require.MaxLevel then
+		HelpInfo(Player, NPC, Anomaly.Conf.Text.NPC.BelowLevel:format(Anomaly.Conf.NameNPC, Anomaly.Conf.Require.MaxLevel, Anomaly.Conf.Name))
+		return LUA_FALSE
+	end
 	return LUA_TRUE
 end
